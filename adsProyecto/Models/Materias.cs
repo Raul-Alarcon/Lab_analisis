a286529e9876ec28a07461274017903e1a0749e8
Raul-Alarcon <128653762+Raul-Alarcon@users.noreply.github.com>
2024-04-07 17:07:38 -0600

    Validacion Materias

--- adsProyecto/Models/Materias.cs
+++ adsProyecto/Models/Materias.cs
@@ -1,8 +1,12 @@
-﻿namespace adsProyecto.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace adsProyecto.Models
 {
     public class Materias
     {
         public int IdMateria { get; set; }
+        [Required(ErrorMessage = "El campo es obligatorio")]
+        [MaxLength(50, ErrorMessage = "El campo no puede tener mas de 50 caracteres")]
         public string nombreMateria{ get; set; }
     }
 }
