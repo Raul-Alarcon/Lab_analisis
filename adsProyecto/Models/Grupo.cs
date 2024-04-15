29907f1701005ca45b8ffec9a7f373254271ca7f
Raul-Alarcon <128653762+Raul-Alarcon@users.noreply.github.com>
2024-04-15 13:44:52 -0600

    correcciones en grupos

--- adsProyecto/Models/Grupo.cs
+++ adsProyecto/Models/Grupo.cs
@@ -1,19 +1,21 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using adsProyecto.Validaciones;
+using System.ComponentModel.DataAnnotations;
 
 namespace adsProyecto.Models
 {
     public class Grupo
     {
+        [CustomRequired(ErrorMessage = "El campo es obligatorio")]
         public int IdGrupo { get; set; }
-        [Required(ErrorMessage = "El campo es obligatorio")]
+        [CustomRequired(ErrorMessage = "El campo es obligatorio")]
         public int IdCarrera { get; set; }
-        [Required(ErrorMessage = "El campo es obligatorio")]
+        [CustomRequired(ErrorMessage = "El campo es obligatorio")]
         public int IdMateria { get; set; }
-        [Required(ErrorMessage = "El campo es obligatorio")]
+        [CustomRequired(ErrorMessage = "El campo es obligatorio")]
         public int IdProfesor { get; set; }
-        [Required(ErrorMessage = "El campo es obligatorio")]
+        [CustomRequired(ErrorMessage = "El campo es obligatorio")]
         public string cliclo { get; set; }
-        [Required(ErrorMessage = "El campo es obligatorio")]
+        [CustomRequired(ErrorMessage = "El campo es obligatorio")]
         public string anio { get; set; }
 
     }
